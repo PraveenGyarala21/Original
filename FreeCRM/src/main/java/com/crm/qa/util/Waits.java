c32c7e1f8178d595e41dda2ae21d2f8a39c616fb
PraveenGyarala21 <52075869+PraveenGyarala21@users.noreply.github.com>
2019-07-17 02:25:57 +0530

    17/07/2019

--- FreeCRM/src/main/java/com/crm/qa/util/Waits.java
+++ FreeCRM/src/main/java/com/crm/qa/util/Waits.java
@@ -1,18 +1,39 @@
 package com.crm.qa.util;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Waits {
 	
 	public static void main(String args[]) {
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\Selenium_Softwares\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Softwares\\chromedriver_win32 (3)\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://mwebware.greythr.com/uas/portal/auth/login?");
+		
 		WebDriverWait wait = new WebDriverWait(driver, 20);
+		
+		WebElement userName  = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[@name='userame']"))));
+		
+		userName.sendKeys("lfjsdlfjls");
+		
+		
 		
 			
 	
